b2ebaa3c3b4c44cd69cd03ba8c38e76ffa6c30d9
demo2 <alexander.morosow@gmail.com>
2022-09-20 03:27:44 -0700

    profile multiple resolutions

--- examples/profile.py
+++ examples/profile.py
@@ -1,34 +1,40 @@
 import torch
+from tqdm import tqdm
+from opensimplex_pytorch.simplex3d import Simplex3D
 
-with torch.profiler.profile(
-    schedule=torch.profiler.schedule(
-        wait=2,
-        warmup=2,
-        active=6,
-        repeat=1),
-    on_trace_ready=torch.profiler.tensorboard_trace_handler('./profile/'),
-    record_shapes=True,
-    with_modules=True,
-    profile_memory=True,
-    with_stack=True,
-    with_flops=True
-) as profiler:
-    from opensimplex_pytorch.simplex3d import simplex3d
+DEVICE = 'cuda:0'
 
-    extent = 20
-    resolution = 2048
-    for _ in range(10):
-
-        with torch.no_grad():
-            xs = torch.linspace(-extent, extent, steps=resolution)
-            ys = torch.linspace(-extent, extent, steps=resolution)
-            x, y = torch.meshgrid(xs, ys, indexing='xy')
-            t = torch.zeros((resolution, resolution))
-            for dt in range(0, 1, 100000):
-                xyt = torch.dstack((x, y, t + dt))
-                flat_xyt = torch.reshape(xyt, (resolution**2, -1))
-                image = simplex3d(flat_xyt)
-        profiler.step()
-
-print(profiler.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=5))
+for lod in range(4, 11):
+    resolution = 2**lod
+    with torch.profiler.profile(
+        schedule=torch.profiler.schedule(
+            wait=1,
+            warmup=100,
+            active=900,
+            repeat=1),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler(f'./logs/{DEVICE}-{resolution}'),
+        record_shapes=True,
+        with_modules=True,
+        profile_memory=True,
+        with_stack=True,
+        with_flops=True
+    ) as profiler:
+        extent = resolution / 4
+        device = torch.device(DEVICE)
+        simplex3d = Simplex3D(device)
+        xs = torch.linspace(-extent, extent, steps=resolution, device=device)
+        ys = torch.linspace(-extent, extent, steps=resolution, device=device)
+        x, y = torch.meshgrid(xs, ys, indexing='xy')
+        x = x.to(device)
+        y = y.to(device)
+        t = torch.zeros((resolution, resolution), device=device)
+        pbar = tqdm(total=1000, desc=f'{resolution}x{resolution}')
+        for dt in torch.arange(0, 1, 1/1000, device=device):
+            xyt = torch.dstack((x, y, t + dt))
+            flat_xyt = torch.reshape(xyt, (resolution**2, -1))
+            image = simplex3d(flat_xyt)
+            pbar.set_postfix(std_mean="%0.2f/%0.2f" % torch.std_mean(image, unbiased=False))
+            pbar.update()
+            profiler.step()
+        del x, y, t
 
